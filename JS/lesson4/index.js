6f5b60ce962be3adeb2b8dfd771bf9130ea9817c
azamjonbro <azamjondasturchi11@gmail.com>
2025-05-23 20:54:36 +0500

    added

---
+++ JS/lesson4/index.js
@@ -0,0 +1,48 @@
+let a = 10
+
+let b = 11
+
+// teskshiruv operatorlari
+
+// dan < katta
+// katta> dan
+// katta >= dan yoki teng
+// dan <= katta yoki teng
+
+
+//if yani agar shart boshlash kalit so'zi hisoblanib undan song () shunday qavs ichiga shart yoziladi 
+// {} bu scope ichiga esa amal yoziladi
+
+
+// elseda esa hech qanday amal yozishimiz uchun scope yozish shart emas
+// if(a>b){
+//  console.log("Ha a katta");
+// }
+// else if(a<b){
+//  console.log("Ha b katta");
+// }
+// else{
+//  console.log("yo'q bu teng");
+// }
+
+// // bu tekshirish operatori
+
+// a==b?console.log("salom bolalar") : console.log("Hello world")
+
+// a==b ?? console.log("Assalomu alaykum") 
+
+// let son = prompt("Son yozing") // prompt bu foydalanubchi son kiritishi uchun chiqadigan modal menu
+
+// if(son==0){
+//  alert("Son  0 ga teng")
+// }
+// else if(son%2==0){
+//  alert("Juft son")
+// }
+// else{
+//   alert("Toq son")
+// }
+
+let ism =prompt("ismingizni yozing")
+
+alert("Assalomu alaykum " + ism)
